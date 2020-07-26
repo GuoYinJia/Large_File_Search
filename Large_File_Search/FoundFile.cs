7dc10a7ece898e8deaaf3ed872ef5f3ca400813a
GuoYinJia <1987537923@qq.com>
2020-07-26 16:17:01 +0800

    大文件搜索

---
+++ Large_File_Search/FoundFile.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 大文件搜索
+{
+    /// <summary>
+    /// 代表找到一个文件
+    /// </summary>
+    class FoundFile : INotifyPropertyChanged
+    {
+        private string name;
+
+        /// <summary>
+        /// 名称
+        /// </summary>
+        public string Name
+        {
+            get { return name; }
+            set 
+            {
+                if (name != value)
+                {
+                    OnPropertyChanged("Name");
+                    name = value;
+                }
+            }
+        }
+
+        private long length;
+
+        /// <summary>
+        /// 大小
+        /// </summary>
+        public long Length
+        {
+            get { return length; }
+            set
+            {
+                if (length != value)
+                {
+                    OnPropertyChanged("Length");
+                    length = value;
+                }
+            }
+        }
+
+        private string location;
+
+        /// <summary>
+        /// 位置
+        /// </summary>
+        public string Location
+        {
+            get { return location; }
+            set
+            {
+                if (location != value)
+                {
+                    OnPropertyChanged("Location");
+                    location = value;
+                }
+            }
+        }
+
+        //事件委托
+        public event PropertyChangedEventHandler PropertyChanged;
+       
+        /// <summary>
+        /// 改变资源时发生
+        /// </summary>
+        /// <param name="name"></param>
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(name));
+            }
+        }
+    }
+}
---
+++ Large_File_Search/NumberConverter.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace 大文件搜索
+{
+    /// <summary>
+    /// 对数字进行转换
+    /// </summary>
+    class NumberConverter : IValueConverter
+    {
+        /// <summary>
+        /// 根据值大小返回不同的表达形式，比如KB，MB，GB
+        /// 大的数字用逗号作为千分位间隔符
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter"></param>
+        /// <param name="culture"></param>
+        /// <returns></returns>
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            long longValue = (long)value;
+
+            double temp = 0;
+            //大于1k,小于1M
+            if ((longValue >= 1024) && (longValue < 1024 * 1024))
+            {
+                temp = (double)longValue / 1024;
+                return temp.ToString("N") + "K";
+            }
+            //大于1M，小于1G
+            if ((longValue >= 1024 * 1024) && (longValue < 1024 * 1024 * 1024))
+            {
+                temp = (double)longValue / (1024 * 1024);
+                return temp.ToString("N") + "M";
+            }
+            //大于1G
+            if ((longValue >= 1024 * 1024 * 1024))
+            {
+                temp = (double)longValue / (1024 * 1024 * 1024);
+                return temp.ToString("N") + "G";
+            }
+            //小于1K的，返回真实大小
+            return longValue.ToString() + "字节";
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
